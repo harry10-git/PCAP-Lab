688baf26bb58ca14b9c5d376802ea0b660a84e8d
Harry Raj <113687752+harry10-git@users.noreply.github.com>
2023-11-14 16:16:51 +0530

    Update q1_wordCount.cu

--- PPL_Practice/week6/q1_wordCount.cu
+++ PPL_Practice/week6/q1_wordCount.cu
@@ -7,13 +7,36 @@
 #include<string.h>
 
 
-__global__ void word_Counts(char * str, int * index, char * word)
+__global__ void word_Counts(char * str, int * index, char * word, int * d_count)
 {
     int tid = threadIdx.x;
     int word_len = strlen(word);
 
     int start = tid*2;
     int end = start +1;
+
+    int len = start - end +1;
+
+    if(len != word_len)
+    {
+        return;
+    }
+
+    bool flag = true;
+    
+    for(int i=0 ; i<word_len; i++)
+    {
+        if(str[start+i] != word[i])
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag)
+    {
+        atomicAdd(d_count,1);
+    }
 
 
 
@@ -53,6 +76,27 @@ int main(void)
         printf("%d ", index[i]);
     }
 
+
+
+
+    //////////////
+    char * d_str;
+    char * d_word;
+    int count = 0;
+    int *d_count;
+    char word[10] = "hii";
+
+
+    cudaMalloc((void**)&d_str, strlen(str)*sizeof(char));
+    cudaMalloc((void**)&d_count, sizeof(int));
+
+    cudaMemcpy(d_str, str,strlen(str)*sizeof(char),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_count, &count,sizeof(int),cudaMemcpyHostToDevice);
+
+    int num_words =  (ptr+1)/2;
+
+   
+    word_Counts<<<1, num_words>>>(d_str, index, )
 
 
 }
